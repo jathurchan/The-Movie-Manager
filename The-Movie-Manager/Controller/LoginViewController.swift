55039290984de571d73cdf7fe0ed4c0fa6a8a0fe
Jathurchan Selvakumar <mail@jathurchan.com>
2021-04-20 13:15:14 +0200

    Updated Storyboard

---
+++ The-Movie-Manager/Controller/LoginViewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.swift
+//  The-Movie-Manager
+//
+//  Created by Jathurchan Selvakumar on 15/04/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
